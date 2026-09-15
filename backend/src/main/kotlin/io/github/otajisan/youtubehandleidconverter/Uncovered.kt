51588e2ce6dff82003be57388a1a2e83f3bd1c78
otajisan <taji.taji.otajisan@gmail.com>
2026-09-15 15:19:56 +0900

    test(ci): [検証] カバレッジ閾値未達で backend-check が失敗すること

---
+++ backend/src/main/kotlin/io/github/otajisan/youtubehandleidconverter/Uncovered.kt
@@ -0,0 +1,30 @@
+package io.github.otajisan.youtubehandleidconverter
+
+/** [検証用] テストされないコードでカバレッジを閾値未満に落とす。 */
+class Uncovered {
+    fun a(x: Int): Int {
+        var r = x
+        r += 1
+        r *= 2
+        r -= 3
+        r /= 1
+        r += 4
+        r *= 5
+        r -= 6
+        r += 7
+        r *= 8
+        r -= 9
+        r += 10
+        r *= 11
+        r -= 12
+        r += 13
+        r *= 14
+        r -= 15
+        r += 16
+        r *= 17
+        r -= 18
+        r += 19
+        r *= 20
+        return r
+    }
+}
